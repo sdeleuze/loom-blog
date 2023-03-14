3bc9a32cf28cfbde131b5aba2835e25d5f5d7e0a
Sébastien Deleuze <sdeleuze@vmware.com>
2023-03-14 10:41:38 +0100

    Fix benchmarks

--- loom-test-overhead.sh
+++ loom-test-overhead.sh
@@ -7,12 +7,12 @@ do
   for EXP in 8
   do
     # for PORT in 8080 8081
-    for PORT in 8082
+    for PORT in 8080 8081
     do
       # for RUN in 1 2 3 4 5 6 7 8 9 10 11
       for RUN in 1 2
       do
-        RESULT=`wrk -t$CONCURRENCY -c$CONCURRENCY -d10s http://localhost:$PORT/loom/overhead?exp=$EXP | grep Requests | awk '{print $2}'`
+        RESULT=`wrk -t$CONCURRENCY -c$CONCURRENCY -d10s http://localhost:$PORT/overhead?exp=$EXP | grep Requests | awk '{print $2}'`
         if [ $RUN -ne 1 ]; then
           echo $CONCURRENCY $EXP $PORT $RESULT
         fi
--- loom-test-simplicity.sh
+++ loom-test-simplicity.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 
+benchmark () {
+   RESULT=`wrk -t$1 -c$1 -d10s http://localhost:$2/$3 | grep Requests | awk '{print $2}'`
+   if [ $4 -ne 1 ]; then
+     echo $1 $2-$3 $RESULT
+   fi
+}
+
 for CONCURRENCY in 1 2 4 8 16 32
 do
   for PORT in 8080 8081
   do
-    for RUN in 1 2 3 4 5 6 7 8 9 10 11
+    #for RUN in 1 2 3 4 5 6 7 8 9 10 11
+    for RUN in 1 2
     do
       if [ $PORT -eq 8080 ]; then
-        SERVLET=async
+        benchmark $CONCURRENCY $PORT blocking $RUN
+        benchmark $CONCURRENCY $PORT async $RUN
       else
-        SERVLET=blocking
-      fi
-      RESULT=`wrk -t$CONCURRENCY -c$CONCURRENCY -d10s http://localhost:$PORT/loom/$SERVLET | grep Requests | awk '{print $2}'`
-      if [ $RUN -ne 1 ]; then
-        echo $CONCURRENCY $PORT $RESULT
+        benchmark $CONCURRENCY $PORT blocking $RUN
       fi
     done
   done
